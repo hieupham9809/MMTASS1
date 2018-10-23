b7784ed65d58aad073e12323640de13aca2afe46
hieupham <hieupham9809@gmail.com>
2018-10-24 01:17:04 +0700

    get rid of bug

--- MiChat/app/src/main/java/michat/model/Message.java
+++ MiChat/app/src/main/java/michat/model/Message.java
@@ -5,11 +5,10 @@ import android.support.annotation.Nullable;
 import com.stfalcon.chatkit.commons.models.IMessage;
 import com.stfalcon.chatkit.commons.models.IUser;
 import com.stfalcon.chatkit.commons.models.MessageContentType;
-
 import java.io.Serializable;
 import java.util.Date;
 
-public class Message implements IMessage, MessageContentType.Image{
+public class Message implements IMessage, MessageContentType.Image {
     String id;
     String text;
     User owner;
@@ -18,37 +17,47 @@ public class Message implements IMessage, MessageContentType.Image{
     Date readAt;
     String imageUrl;
 
-    public Message(String id, String text, User owner, Date createdAt, Date receivedAt,Date readAt) {
+    public Message(String id, String text, User owner, Date createdAt, Date receivedAt, Date readAt) {
         this.id = id;
         this.text = text;
         this.owner = owner;
         this.createdAt = createdAt;
         this.receivedAt = receivedAt;
-        this.readAt=readAt;
-        imageUrl=null;
+        this.readAt = readAt;
+        imageUrl = null;
     }
+
     @Override
     public String getId() {
         return id;
     }
+
     @Override
     public String getText() {
         return text;
     }
+
     @Override
     public User getUser() {
         return owner;
     }
+
     public Date getCreatedAt() {
         return createdAt;
     }
+
     public Date getReceivedAt() {
         return receivedAt;
     }
-    public Date getReadAt(){return readAt;}
-    public void setImageUrl(String image){
-        imageUrl=image;
+
+    public Date getReadAt() {
+        return readAt;
     }
+
+    public void setImageUrl(String image) {
+        imageUrl = image;
+    }
+
     @Nullable
     @Override
     public String getImageUrl() {
